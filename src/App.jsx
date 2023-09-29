1a4812decedde033165c117350bdb84696c52a6d
bahare <pondblue07@gmail.com>
2023-09-29 16:22:15 +0330

    add an users array to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
 import Box from "./Box"
 
 function App(){
+
+    let users = [
+        {
+            id : 1,
+            firstName : 'milad',
+            lastName : 'bahrami',
+            age : 26
+        },
+        {
+            id : 2,
+            firstName : 'bahare',
+            lastName : 'sedigh',
+            age : 16
+        },
+        {
+            id : 3,
+            firstName : 'mina',
+            lastName : 'salami',
+            age : 19
+        },
+        {
+            id : 4,
+            firstName : 'amir',
+            lastName : 'kazemi',
+            age : 28
+        }
+    ]
     return(
         <>
-        <Box />
+        {users.map( user => <Box {...user} />)}
         </>
     )
 }
