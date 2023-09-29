69195581f2396f54cf59255f7a964aa2ab92a16b
bahare <pondblue07@gmail.com>
2023-09-29 16:02:48 +0330

    add a div that contain user's informations

--- src/Box.jsx
+++ src/Box.jsx
@@ -2,6 +2,11 @@ import './Box.css'
 function Box(){
     return(
         <>
+        <div className="box">
+            <p>first name : milad</p>
+            <p>last name : bahrami</p>
+            <p>age : 26</p>
+        </div>
         </>
     )
 }
